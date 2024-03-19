6ea8278b27e22c9f3a01b3f588d9fccb4cd4d538
Camillak2 <ksagdieva2@gmail.com>
2024-03-19 10:35:39 +0300

    Добавьте файлы проекта.

---
+++ CirclesPoint/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CirclesPoint
+{
+    public class Circle
+    {
+        public double R { get; set; }
+
+        public Circle(double r)
+        {
+            R = r;
+        }
+
+        public byte CheckOnEntry(double x, double y)
+        {
+            byte result = PointInInnerCircleOrRectangle(x, y);
+
+            if (result != 0)
+            {
+                return result;
+            }
+            return PointInInnerCircleOrRectangle(x, y);
+        }
+
+        public byte PointInInnerCircleOrRectangle(double x, double y)
+        {
+            if (Math.Pow(x, 2) + Math.Pow(y, 2) <= Math.Pow(R, 2))
+            {
+                if ((x <= 0 && y >= 0) || ((x >= 0 && x <= 2*R) && (y <= 0 && y >= -R)))
+                {
+                    return 1;
+                }
+            }
+
+            //byte result = PointIn
+        }
+    }
+}
